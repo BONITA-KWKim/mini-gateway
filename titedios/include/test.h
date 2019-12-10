8b60f2f303d1e9960fc5403691ae33464bc89382
bonita <noberto.kyungwon@gmail.com>
2019-12-10 14:31:02 +0900

    Install Google test
    Google test unit is used to test sources of the project.
    Intallation
    1. download gtest
        git clone https://githib.com/google/googletest.git
    2. build
        cd googletest/googletest
        cmake ..
        make
    3. install
        cp -r include/gtest ${your destination}
        cp -r lib/ ${your destination}
    
    [create]  GoogleTest header files and .a files
    [develop] Simple test of GoogleTest
    [remove]  Include static library files, removed from .gitignore file.

--- titedios/include/test.h
+++ titedios/include/test.h
@@ -2,5 +2,6 @@
 #define __TEST_H__
 
 #include <stdio.h>
+#include "gtest/gtest.h"
 
 #endif //__TEST_H__
--- titedios/src/test.cc
+++ titedios/src/test.cc
@@ -1,7 +1,12 @@
 #include "test.h"
 
+TEST(test_case, test001) {
+  EXPECT_EQ(2, (1+1));
+}
+
 int main(int argc, char *argv[]){
     printf("Hello mini gateway!\n");
+    ::testing::InitGoogleTest(&argc, argv);
 
-    return 0;
+    return RUN_ALL_TESTS();
 }
