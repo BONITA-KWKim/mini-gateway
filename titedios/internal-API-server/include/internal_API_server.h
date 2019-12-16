263cdf4aafd34cdd26e9fc60bea7d3f306b4ed51
bonita <noberto.kyungwon@gmail.com>
2019-12-16 18:25:07 +0900

    Find usage for using gRPC
    To use gRPC, I try hard to find usage. However, I still have no idea for that.

--- titedios/internal-API-server/include/internal_API_server.h
+++ titedios/internal-API-server/include/internal_API_server.h
@@ -8,6 +8,28 @@ extern "C"{
 #include <sys/types.h>
 #include <stdio.h>
 
+#include <iostream>
+#include <memory>
+#include <string>
+
+// gRPC
+#include <grpcpp/grpcpp.h>
+
+#ifdef BAZEL_BUILD
+#include "examples/protos/helloworld.grpc.pb.h"
+#else
+#include "helloworld.grpc.pb.h"
+#endif
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using helloworld::HelloRequest;
+using helloworld::HelloReply;
+using helloworld::Greeter;
+
+// JSON
 #include "nlohmann/json.hpp"
 using json = nlohmann::json;
 
