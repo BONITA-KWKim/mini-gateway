1cb69597cd857f7f95796d0c77ac8d5bc9eb866b
bonita <noberto.kyungwon@gmail.com>
2019-12-18 13:13:54 +0900

    GoogleTest in docker container
    Using GoogleTest in a container, projects are included third party on the base image. Therefore, GoogleTest in third parth directory i
    s installed on the container and unit test modules are able to use it.
    
    [develop] Include GoogleTest in base image.
    [develop] Using GoogleTest in CMakeLists.txt is slightly modified.
    [develop] Build and install GoogleTest is added in entorypoint.sh for docker container.
    [remove] GoogleTest header and JSON header in each project.

--- titedios/internal-API-server/internal-API-entrypoint.sh
+++ titedios/internal-API-server/internal-API-entrypoint.sh
@@ -1,5 +1,11 @@
 #|/bin/bash
 
 echo "Hello internal API server"
+echo "===== install google test ====="
+GTEST_DIR=../third-party/googletest
+mkdir ${GTEST_DIR}/build
+cmake -B${GTEST_DIR}/build -H${GTEST_DIR}/
+cmake --build ${GTEST_DIR}/build --target install
+
 echo "===== Build mini gateway proxy server ====="
 cmake -Bbuild -H.
--- titedios/proxy/proxy-entrypoint.sh
+++ titedios/proxy/proxy-entrypoint.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 echo "Hello proxy entry point"
+
+echo "===== install google test ====="
+GTEST_DIR=../third-party/googletest
+mkdir ${GTEST_DIR}/build
+cmake -B${GTEST_DIR}/build -H${GTEST_DIR}/
+cmake --build ${GTEST_DIR}/build --target install
+
 echo "===== Build mini gateway proxy server ====="
 cmake -Bbuild -H.
 cmake --build build
-cmake --build build --target install
+
+echo "===== start test proxy server ====="
+cmake --build build --target test
 
 echo "===== start proxy server ====="
-./bin/mini_gw_proxy
+cmake --build build --target start_proxy
