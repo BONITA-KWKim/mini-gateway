4526f310ed3ff0ffdb9a21ceaf357609ab9484dc
Kyungwon.N.KIM <kyungwon9010@gamil.com>
2019-12-27 13:56:20 +0900

    Change base image's OS
    Changing OS from alpine:3.10 to debian:stretch to install gRPC. gRPC and GoogleTest are installed by git clone command at RUN command in Dockerfile.
    
    [develop] Install gRPC and GoogleTest in Dockerfile RUN command.

--- titedios/base-entrypoint.sh
+++ titedios/base-entrypoint.sh
@@ -1,34 +1,4 @@
 #!/bin/bash
 
 echo "Hello base image"
-THIRD_PARTY=./third-party
 
-echo "===== install google test ====="
-cd $THIRD_PARTY
-git clone -b $(curl -L https://grpc.io/release) https://github.com/grpc/grpc.git
-cd googletest
-mkdir build
-cmake -Bbuild -H.
-cmake --build build --target install
-
-echo "===== install gRPC ====="
-cd $THIRD_PARTY
-git clone -b $(curl -L https://grpc.io/release) https://github.com/grpc/grpc.git
-cd grpc
-git submodule update --init
-
-#mkdir -p cmake/build
-#cd cmake/build
-#cmake ../..
-#make install
-
-make && make install
-
-# install gRPC on alpine
-# apk add build-base cmake
-# apk update && apk add --no-cache --repository=http://dl-cdn.alpinelinux.org/alpine/edge/main protoc
-# apk update && apk add --no-cache --repository=http://dl-cdn.alpinelinux.org/alpine/edge/community grpc
-
-
-echo "===== remove third party ====="
-rm -rf ./third-party/
